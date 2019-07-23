3e17dfd516464cd66ef78d47f42cbade800d9b09
zhucb <zhucb@bsfit.com.cn>
2019-07-23 17:15:04 +0800

    只生成一次backJson文件

--- extension.js
+++ extension.js
@@ -1,26 +1,26 @@
-const { registerCommand, msg } = require("./utils/vs");
-const { plugin } = require("./lib/constant");
-const flat = require("./lib/flat");
+const { registerCommand, msg } = require('./utils/vs');
+const { plugin } = require('./lib/constant');
+const flat = require('./lib/flat');
 function activate(context) {
-  //   msg(plugin.congratulations);
-  let disposable = registerCommand("extension.vueSwiftI18n.flatJson", uri => {
-    if (uri && uri.path) {
-      flat(uri)
-    } else {
-      msg.warn(plugin.noUri);
-    }
-  });
+    //   msg(plugin.congratulations);
+    let disposable = registerCommand('extension.vueSwiftI18n.flatJson', uri => {
+        if (uri && uri.path) {
+            flat(uri);
+        } else {
+            msg.warn(plugin.noUri);
+        }
+    });
 
-  context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 // exports.activate = activate;
 
 // this method is called when your extension is deactivated
 function deactivate() {
-  msg.info(`${plugin.name} deactivated`);
+    msg.info(`${plugin.name} deactivated`);
 }
 
 module.exports = {
-  activate,
-  deactivate
+    activate,
+    deactivate,
 };
