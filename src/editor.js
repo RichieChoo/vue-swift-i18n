76bdf74fe30cf98df0279d90a46b40a2ff1a5ffd
zhucb <zhucb@bsfit.com.cn>
2019-08-02 13:10:51 +0800

    添加todo

--- src/editor.js
+++ src/editor.js
@@ -65,18 +65,21 @@ const varifyFile = filePath => {
     if (!fs.existsSync(filePath)) {
         //TODO: 跳转到国际化设置路径!
         msg.error(`Not Found File:${filePath}`);
+        return false;
+    } else {
+        //TODO: 跳转查看该文件!
+        msg.info(`Get Locales Path:${filePath}`);
+        return filePath;
     }
-    //TODO: 跳转查看该文件!
-    msg.info(`Get Locales Path:${filePath}`);
-    return filePath;
 };
 const getLocales = (fsPath, defaultLocalesPath) => {
     const dirName = path.dirname(fsPath);
-    if (!!defaultLocalesPath) {
-        const jsonPath = path.join(defaultLocalesPath, 'zh-cn.json');
-        return varifyFile(jsonPath);
-    } else if (fs.existsSync(path.join(dirName, 'package.json'))) {
-        const jsonPath = path.join(dirName, 'src', 'locales', 'zh-cn.json');
+    if (fs.existsSync(path.join(dirName, 'package.json'))) {
+        let jsonPath = path.join(dirName, 'src', 'locales', 'zh-cn.json');
+        if (!!defaultLocalesPath) {
+            //TODO: 兼容绝对路径!
+            jsonPath = path.join(dirName, defaultLocalesPath, 'zh-cn.json');
+        }
         return varifyFile(jsonPath);
     } else {
         return getLocales(dirName, defaultLocalesPath);
@@ -103,79 +106,79 @@ module.exports = editor => {
     if (!localesPath) return;
     const localeObj = changeObjeValueKey(flatten(require(localesPath)));
     const lines = [];
-    // for (let i = 0; i < lineCount; i++) {
-    //     //使用text替换,getWordRangeAtPosition无法替换全部
-    //     const line = editor.document.lineAt(i);
-    //     let lineText = line.text || '';
-    //     console.warn('lineText', typeof lineText);
-    //     if (
-    //         (!range.template.begin &&
-    //             range.template.begin !== 0 &&
-    //             range.template.end) ||
-    //         (range.template.begin &&
-    //             !range.template.end &&
-    //             range.template.end !== 0)
-    //     ) {
-    //         msg.error('当前vue文件template标签不完整');
-    //         return;
-    //     }
-    //     if (
-    //         (!range.script.begin &&
-    //             range.script.begin !== 0 &&
-    //             range.script.end) ||
-    //         (range.script.begin && !range.script.end && range.script.end !== 0)
-    //     ) {
-    //         msg.error('当前vue文件script标签不完整');
-    //         return;
-    //     }
+    for (let i = 0; i < lineCount; i++) {
+        //使用text替换,getWordRangeAtPosition无法替换全部
+        const line = editor.document.lineAt(i);
+        let lineText = line.text || '';
+        console.warn('lineText', typeof lineText);
+        if (
+            (!range.template.begin &&
+                range.template.begin !== 0 &&
+                range.template.end) ||
+            (range.template.begin &&
+                !range.template.end &&
+                range.template.end !== 0)
+        ) {
+            msg.error('当前vue文件template标签不完整');
+            return;
+        }
+        if (
+            (!range.script.begin &&
+                range.script.begin !== 0 &&
+                range.script.end) ||
+            (range.script.begin && !range.script.end && range.script.end !== 0)
+        ) {
+            msg.error('当前vue文件script标签不完整');
+            return;
+        }
 
-    //     //{{$t("xx")}}   template下 html替换
-    //     if (templateTextRegexp.test(lineText)) {
-    //         lineText = lineText.replace(templateTextRegexp, str => {
-    //             const result = localeObj[str];
-    //             if (result) {
-    //                 return "{{$t('" + result + "')}}";
-    //             }
-    //             return str;
-    //         });
-    //         console.warn('lineText', lineText);
-    //     }
+        //{{$t("xx")}}   template下 html替换
+        if (templateTextRegexp.test(lineText)) {
+            lineText = lineText.replace(templateTextRegexp, str => {
+                const result = localeObj[str];
+                if (result) {
+                    return "{{$t('" + result + "')}}";
+                }
+                return str;
+            });
+            console.warn('lineText', lineText);
+        }
 
-    //     //$t("xx")   template下 属性替换
-    //     if (propertyRegexp.test(lineText)) {
-    //         lineText = lineText.replace(propertyRegexp, str => {
-    //             console.warn('str', str);
-    //             const prefix = str.split('=')[0].replace(/\s/g, ' :');
+        //$t("xx")   template下 属性替换
+        if (propertyRegexp.test(lineText)) {
+            lineText = lineText.replace(propertyRegexp, str => {
+                console.warn('str', str);
+                const prefix = str.split('=')[0].replace(/\s/g, ' :');
 
-    //             const mainStr = str.split('=')[1].replace(/[\"|\']/g, '');
-    //             const result = localeObj[mainStr];
-    //             if (result) {
-    //                 return `${prefix}="$t('${result}')"`;
-    //             }
-    //             return str;
-    //         });
-    //     }
-    //     //this.$t("xx")   script下 替换
-    //     if (scriptRegexp.test(lineText)) {
-    //         if (!range.template.end || i > range.template.end) {
-    //             lineText = lineText.replace(scriptRegexp, str => {
-    //                 const resultStr = str.replace(/[\"|\']/g, '');
-    //                 const result = localeObj[resultStr];
-    //                 if (result) {
-    //                     return "this.$t('" + result + "')";
-    //                 }
-    //                 return str;
-    //             });
-    //         }
-    //         console.warn('lineText', lineText);
-    //     }
-    //     lines.push(lineText);
-    // }
-    // const editText = lines.join('\n');
-    // editor.edit(editBuilder => {
-    //     const end = new Position(lineCount + 1, 0);
-    //     editBuilder.replace(new Range(new Position(0, 0), end), editText);
-    // });
+                const mainStr = str.split('=')[1].replace(/[\"|\']/g, '');
+                const result = localeObj[mainStr];
+                if (result) {
+                    return `${prefix}="$t('${result}')"`;
+                }
+                return str;
+            });
+        }
+        //this.$t("xx")   script下 替换
+        if (scriptRegexp.test(lineText)) {
+            if (!range.template.end || i > range.template.end) {
+                lineText = lineText.replace(scriptRegexp, str => {
+                    const resultStr = str.replace(/[\"|\']/g, '');
+                    const result = localeObj[resultStr];
+                    if (result) {
+                        return "this.$t('" + result + "')";
+                    }
+                    return str;
+                });
+            }
+            console.warn('lineText', lineText);
+        }
+        lines.push(lineText);
+    }
+    const editText = lines.join('\n');
+    editor.edit(editBuilder => {
+        const end = new Position(lineCount + 1, 0);
+        editBuilder.replace(new Range(new Position(0, 0), end), editText);
+    });
 };
 
 //TODO:
