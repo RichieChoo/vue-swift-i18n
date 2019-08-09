8386304edb12e81a6e1838552846ab85775abf22
zhucb <zhucb@bsfit.com.cn>
2019-08-09 18:28:59 +0800

    update regexp

--- utils/regex.js
+++ utils/regex.js
@@ -1,8 +1,6 @@
-
 //当看到这个页面，说明你对Regexp赋值了↓↓↓
 //欢迎使用JavaScript中的RegExp的众多陷阱之一，当flag 是global时，第二次使用它将会从第一次匹配到的lastIndex开始查。。。
 //所以只要是变量赋值多次使用的Regexp，都需要使用match，或者在使用完test,exec之后重置lastIndex
-
 
 //约定:所有汉字匹配均以汉字开头,所有正则针对 单行匹配
 const spaceRegexp = /\s/g;
@@ -14,7 +12,6 @@ const templateEndRegexp = /\<\/template\>/g;
 const scriptBeginRegexp = /\<script/g;
 const scripteEndRegexp = /\<\/script/g;
 
-
 //只匹配单行注释，多行注释不考虑
 const commentRegexp = /\/\//g;
 
@@ -25,28 +22,33 @@ const scriptRegexp = /((?<!=)["|'][\u4e00-\u9fa5]["|'])|((?<!=)["|'][\u4e00-\u9f
 const propertyRegexp = /(\s\w+=["|'][\u4e00-\u9fa5]\S+["|'])|(\s\w+=["|'][\u4e00-\u9fa5]["|'])/g;
 
 // 单行  匹配 template ><下的汉字
-const templateTextInAngleBracketsRegexp = /(\>[\u4e00-\u9fa5]\S+\<)|(\>[\u4e00-\u9fa5]\<)/g;
+// const templateTextInAngleBracketsRegexp = /(\>[\u4e00-\u9fa5]\S+\<)|(\>[\u4e00-\u9fa5]\<)/g;
+const templateTextInAngleBracketsRegexp = /(?<=[>\s])[\u4e00-\u9fa5]\S+(?=[\s<])|(?<=[>\s])[\u4e00-\u9fa5](?=[\s<])/g;
 
-// 单行  匹配 ，配合template range 判断 是否是template中的空字符开头的 汉字
+// 单行  匹配 ，配合template range ,warnRegexp判断 是否是template中的空字符开头的 汉字
 const templateTextInLineRegexp = /((?<=\s+)[\u4e00-\u9fa5]\S+)|((?<=\s+)[\u4e00-\u9fa5])/g;
+
+//匹配到特殊字符串说明前面正则匹配有问题，给出提示，去掉匹配
+const warnRegexp = /[{}<>:]/g;
 
 // 匹配多行 $t替换的字符串
 const dollarTRegexp = /(?<=\$t\(["'])[^'"]+/gm;
 module.exports = {
-    templateBeginRegexp,
-    templateEndRegexp,
-    scriptBeginRegexp,
-    scripteEndRegexp,
+  templateBeginRegexp,
+  templateEndRegexp,
+  scriptBeginRegexp,
+  scripteEndRegexp,
 
-    scriptRegexp,
-    propertyRegexp,
-    templateTextInLineRegexp,
-    templateTextInAngleBracketsRegexp,
-    angleBracketsRegexp,
-    quotationRegexp,
-    spaceRegexp,
-    firstSpaceRegexp,
-    commentRegexp,
+  scriptRegexp,
+  propertyRegexp,
+  templateTextInLineRegexp,
+  templateTextInAngleBracketsRegexp,
+  warnRegexp,
+  angleBracketsRegexp,
+  quotationRegexp,
+  spaceRegexp,
+  firstSpaceRegexp,
+  commentRegexp,
 
-    dollarTRegexp
+  dollarTRegexp
 };
