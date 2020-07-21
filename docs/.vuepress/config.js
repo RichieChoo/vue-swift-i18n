553bd0f5d1495cc275a04a6d451547d1856690f1
zhucb <zhucb@bsfit.com.cn>
2020-07-21 11:43:40 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,7 @@
 module.exports = {
     title: 'Vue Swift I18n',
-    description: 'vscode 极速国际化插件（vue-i18n）'
+    description: 'vscode 极速国际化插件（vue-i18n）',
+    head: [
+      ['link', { rel: 'icon', href: '/logo.png' }]
+    ]
   }
