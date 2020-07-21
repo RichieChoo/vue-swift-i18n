12a72fa146538be16d763908d09ea948e587023a
zhucb <zhucb@bsfit.com.cn>
2020-07-21 09:44:14 +0800

    添加部署脚本

---
+++ deploy-docs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+deployName="Vue Swift I18n Docs"
+readonly deployName
+
+echo "Deploying '${deployName}'..."
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 清除对项目的改动
+git clean -fd
+
+# 获取最新代码
+git pull origin master
+
+# 生成静态文件
+npm run docs:build
+
+# 重启nginx服务器
+nginx -s reload
